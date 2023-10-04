c4702913aea4ac69dc1bfb34e0015ae3e2be3ff7
islamkabbary <islamkabbary@gmail.com>
2023-10-05 00:33:28 +0300

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\LanguageMiddleware;
 
 class Kernel extends HttpKernel
 {
@@ -36,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Routing\Middleware\LanguageMiddleware::class,
+            LanguageMiddleware::class,
         ],
 
         'api' => [
@@ -64,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'lang' => \Illuminate\Routing\Middleware\LanguageMiddleware::class,
+        'lang' => LanguageMiddleware::class,
     ];
 }
