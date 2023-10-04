f23e140b32ed419eff5aba51792b0605f2595a3e
islamkabbary <islamkabbary@gmail.com>
2023-10-05 00:35:41 +0300

    fix

---
+++ app/Http/Controllers/ApartmentDetailsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApartmentDetailsController extends Controller
+{
+    public function index()
+    {
+        return view('web.details');
+    }
+}
